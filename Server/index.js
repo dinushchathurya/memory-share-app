42f89fd64e1a7bcbbd591e8ac15607f0413021fe
dinushchathurya <dinushchathurya21@gmail.com>
2020-12-27 19:24:14 +0530

    :sparkles: Connect MongoDB database to application

---
+++ Server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors')
+
+const app = express();
+
+app.use(bodyParser.json({ limit:"30mb", extended:true}));
+app.use(bodyParser.urlencoded({ limit:"30mb", extended:true}));
+app.use(cors());
+
+/* DataBase Config */
+const db = require('./config/keys').mongoURI;
+
+/* Connect to MongoDB */
+mongoose
+    .connect(db, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
